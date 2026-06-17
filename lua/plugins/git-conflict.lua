3dc6ece250b8cdbd6f1cb03690fb02fb4093f3dd
Bamorial <veltandev@gmail.com>
2026-06-18 00:57:05 +0300

    [GIT CONFLICT]

---
+++ lua/plugins/git-conflict.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    event = "BufReadPre",
+    opts = {
+      default_mappings = false,
+      default_commands = true,
+      disable_diagnostics = false,
+      list_opener = "copen",
+    },
+    keys = {
+      { "<leader>gco", "<cmd>GitConflictChooseOurs<cr>", desc = "Conflict Choose Ours" },
+      { "<leader>gct", "<cmd>GitConflictChooseTheirs<cr>", desc = "Conflict Choose Theirs" },
+      { "<leader>gcb", "<cmd>GitConflictChooseBoth<cr>", desc = "Conflict Choose Both" },
+      { "<leader>gc0", "<cmd>GitConflictChooseNone<cr>", desc = "Conflict Choose None" },
+      { "]x", "<cmd>GitConflictNextConflict<cr>", desc = "Next Conflict" },
+      { "[x", "<cmd>GitConflictPrevConflict<cr>", desc = "Previous Conflict" },
+      { "<leader>gcl", "<cmd>GitConflictListQf<cr>", desc = "Conflict List" },
+    },
+  },
+}
